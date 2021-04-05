bf72c5cdc9b4abcbedd66ae89ae7cd7806cbe7a7
abhi-192 <81365470+abhi-192@users.noreply.github.com>
2021-04-06 03:40:00 +0530

    code clean up

--- Web development/Back-end/NodeJS-Express-MogoDB/using express/index.js
+++ Web development/Back-end/NodeJS-Express-MogoDB/using express/index.js
@@ -7,8 +7,10 @@ const app = express();
 
 //GET,POST,PATCH,PUT,DELETE are mostlky used.
 //use app.post() for POST and so on.
+
 app.get('./index.html',function(req,res){
-    res.send('<hi> Send HTML files here. </hi>');
+    //send html files through this
+    res.send('<hi> Even HTML files can be sent from here ,not only plain text .Send HTML files here. </hi>');
 });
 
 app.listen(port,function(err){
